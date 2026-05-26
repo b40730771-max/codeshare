6abb41948b5326a83d6f4e28f18fd350892471b8
b40730771-max <b40730771@gmail.com>
2026-05-27 08:22:12 +0900

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,7 @@
+export default function sitemap() {
+  return [
+    { url: 'https://codeshare-eta-jade.vercel.app', lastModified: new Date() },
+    { url: 'https://codeshare-eta-jade.vercel.app/members', lastModified: new Date() },
+    { url: 'https://codeshare-eta-jade.vercel.app/search', lastModified: new Date() },
+  ]
+}
